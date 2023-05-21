fa4816e5c77e9f5603cf0eb2d210f845dd8f3eda
Pieter Coetzee <Pieter.Coetzee@Paradigmtech.co.za>
2023-05-21 20:44:27 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Card}/{action=Index}/{id?}");
 
 app.Run();
